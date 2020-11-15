bda485a0e44d2bd492ab01790f936276804c6cca
burnsaustin145 <burnsaustin145@yahoo.com>
2020-11-15 14:44:09 -0500

    for michael

---
+++ for_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(int i=0; i<10; i++) {
+
+	       	cout << i << "\n";
+		for(int j=0; j<10; j++) {
+
+			cout << j << "\n";
+			for(int k=0; k<10; k++) {
+
+				cout << k << "\n";
+			}
+		}
+	}
+	return 0;
+}
